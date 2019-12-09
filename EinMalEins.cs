df926a13f96ab9b0e76da581603893853af3596f
Christian Schadler <c.schadler@HTBLALeonding.onmicrosoft.com>
2019-12-09 23:52:00 +0100

    Add project files.

---
+++ EinMalEins.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EinMalEins.ConsoleApplication
+{
+    class EinMalEins
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 x 1 -Tabelle");
+
+            for(int i = 1; i <=10;i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0,2} mal {1,2} ist {2,4}", j, i, i * j);
+                }
+
+                Console.WriteLine("\n\nBeliebige Taste drücken\n");
+                Console.ReadKey();
+            }
+        }
+    }
+}
